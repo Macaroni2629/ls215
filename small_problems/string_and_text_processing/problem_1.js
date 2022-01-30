047f356dd34aecefbc8f728c71ae0b752319dd61
Rona <RonaHsu@gmail.com>
2022-01-30 15:52:39 -0600

    added problem 1

---
+++ small_problems/string_and_text_processing/problem_1.js
@@ -0,0 +1,16 @@
+// Uppercase Check
+// Write a function that takes a string argument and returns true if all of the alphabetic characters inside the string are uppercase; otherwise, return false. Ignore characters that are not alphabetic.
+
+// Examples:
+
+function isUppercase(string) {
+  return string === string.toUpperCase();
+}
+
+// Copy Code
+console.log(isUppercase('t'));               // false
+console.log(isUppercase('T'));               // true
+console.log(isUppercase('Four Score'));      // false
+console.log(isUppercase('FOUR SCORE'));      // true
+console.log(isUppercase('4SCORE!'));         // true
+console.log(isUppercase(''));                // true
