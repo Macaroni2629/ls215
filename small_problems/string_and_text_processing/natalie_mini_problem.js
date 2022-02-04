577df097818bc39dfb67bed29731a794eca705d6
Rona <RonaHsu@gmail.com>
2022-02-04 16:42:47 -0600

    add natalie mini problem

---
+++ small_problems/string_and_text_processing/natalie_mini_problem.js
@@ -0,0 +1,90 @@
+/*
+
+PROBLEM RESTATED: Given two numbers in SHORTHAND, in string form, find the true value of the second number?
+
+QUESTIONS: 
+1) How do I know for example that the second number is way larger than the second number?  
+    
+
+RULES EXPLICIT: What the numbers represent in reality-- the first number is always lower in value than the second one. 
+
+
+RULES IMPLICIT: 
+  - 2 arguments are always given in string form.
+  - Possible to have leading zeroes.
+
+RULES:
+  -The next largest number from `num1` where the digits shown are the last few digits of that next number `num2`.
+
+EDGE CASES: 
+  - Leading zeroes.
+
+EXAMPLES/TEST CASES:
+HAPPY PATH
+console.log(compareTwoNums('7', '2')) // 12 
+Compare 7 and 2.  Is 7 less than 2?
+No, it's not.
+  Find the length of the first number.
+  Subtract one from it.  Save in variable `power`
+Use first number as starting point.
+Increment by 1 in a loop UNTIL it's equal to the second number + 10 to the `power`
+7 = 12?
+8 = 12?
+9 = 12?
+10 = 12?
+11 = 12?
+12 = 12? YES
+
+
+console.log(compareTwoNums('104', '2')) // 112 
+console.log(compareTwoNums('104', '02')) // 202
+
+console.log(compareTwoNums('1', '2')) // 2
+  
+
+
+UNHAPPY PATH
+-
+
+INPUT(S):
+  - string nums
+OUTPUTS:
+  - number
+ALGORITHM (break into problems + guard clauses):
+  - Define function `compareTwoNums` with two parameters, `num1` and `num2`.
+
+  -Find new value of `num2`
+    -Find the length of num1.  Subtract 1.
+    -Save in variable `power`
+    -Reassign `num2` to itself + 10 ^ power
+  
+  - Start a `while`  loop, with the condition being while `increment` < `num2`.
+    -increment `increment` by 1 until it's equal to `num2` 
+    
+    
+  - Return `num2`
+  
+*/
+
+function compareTwoNums(num1, num2) {
+  
+  let string1 = String(num1)
+  let num1Number = Number(num1)
+  let string2 = String(num2)
+  let num2Number = Number(num2)
+  let sliceSize = string2.length;
+  
+  while (num1Number < 1000) {
+    num1Number += 1
+    string1 = String(num1Number)
+    if (string1.slice(-sliceSize) === string2.slice(-sliceSize)) {
+      return num1Number
+    }
+  }
+  
+}
+
+console.log(compareTwoNums('7', '2')) // 12
+console.log(compareTwoNums('104', '2')) // 112 
+console.log(compareTwoNums('104', '02')) // 202
+console.log(compareTwoNums('1', '2')) // 2
