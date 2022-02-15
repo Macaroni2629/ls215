80bfe387ae1b97d1540eee11399bd5ddbbc6fe84
Rona <RonaHsu@gmail.com>
2022-02-14 18:18:47 -0600

    added triangle sides

---
+++ small_problems/medium_2/triangle_sides.js
@@ -0,0 +1,434 @@
+Rona
+#5658
+
+annemelody — 11/11/2021
+https://docs.google.com/document/d/1iZaqwmfLtZlLWZniCW3UPnNMxBxa0rLxLt_LAbl8FRE/edit?usp=sharing
+Google Docs - create and edit documents online, for free.
+Create a new document and edit with others at the same time -- from your computer, phone or tablet. Get stuff done with or without an internet connection. Use Docs to edit Word files. Free from Google.
+https://docs.google.com/document/d/1iZaqwmfLtZlLWZniCW3UPnNMxBxa0rLxLt_LAbl8FRE/edit?usp=sharing
+Google Docs - create and edit documents online, for free.
+Create a new document and edit with others at the same time -- from your computer, phone or tablet. Get stuff done with or without an internet connection. Use Docs to edit Word files. Free from Google.
+InuNatNat — 11/11/2021
+https://www.protest.eu/
+PROTEST Official Shop | Since 1993 | To get there!
+We are Protest. A Dutch sportswear brand creating functional and trendy clothing for skiing, snowboarding, surfing and swimming. Protest. To get there!
+annemelody — 11/11/2021
+https://prod.liveshare.vsengsaas.visualstudio.com/join?705342C38DFB90CB34E4A7E53ABBF55CC0B0
+Visual Studio Code for the Web
+Build with Visual Studio Code, anywhere, anytime, entirely in your browser.
+annemelody — 11/11/2021
+https://eastmanreference.com/complete-list-of-html-tags
+Complete List of HTML Tags
+A pretty much complete although still pretty basic list of HTML tags in alphabetical order from a to wbr.
+InuNatNat — 11/15/2021
+https://www.youtube.com/watch?v=pCvZtjoRq1I
+YouTube
+Traversy Media
+What Is MVC? Simple Explanation
+Image
+Rona — 11/15/2021
+https://www.youtube.com/watch?v=ZS_kXvOeQ5Y&ab_channel=Academind
+YouTube
+Academind
+SQL vs NoSQL or MySQL vs MongoDB
+Image
+Rona — 11/16/2021
+https://app.coderpad.io/FEXD4EXN
+Ruby | CoderPad
+An online IDE for technical interviews offering a better candidate experience.
+
+InuNatNat — 11/16/2021
+https://krita.org/en/download/krita-desktop/
+Krita
+Scott Petrovic
+Krita Desktop | Krita
+Download the latest version of Krita for FREE on your device. Available on Windows, Linux, OSX, and Android tablets.
+Rona — 11/16/2021
+guys i forgot, i'm not showing up tonight because it's my birthday
+Rona — 11/16/2021
+my family said they'd be half an hour late
+can i be here for 30 minutes?
+InuNatNat — 11/17/2021
+https://html-css-js.com/css/generator/box-shadow/
+Box Shadow CSS Generator
+Set the properties of your box shadow to get the CSS style. Use the sliders and the color picker to set the values and watch the live preview
+Image
+Rona — 11/18/2021
+https://prod.liveshare.vsengsaas.visualstudio.com/join?C8CE381E792C497F617AABCA576C5381C46A
+Visual Studio Code for the Web
+Build with Visual Studio Code, anywhere, anytime, entirely in your browser.
+InuNatNat — 11/22/2021
+npm install -g npm
+annemelody — 12/17/2021
+https://medium.com/@amtiotuico/7f526dbc4283
+Medium
+InuNatNat — 12/17/2021
+https://natdecode.wordpress.com/2021/12/17/truthiness/
+Nat Decode
+Truthiness
+The idea of truthiness is the idea that every value/literal in a language can evaluate to true or false when used inside a conditional.  This is useful in very flexible languages like JavaScri…
+Image
+Rona — 12/17/2021
+https://medium.com/@ronahsu/what-is-the-difference-between-let-const-and-var-in-javascript-a0c6c2ec8807 article1
+Medium
+https://medium.com/@ronahsu/what-are-closures-8b6ae390436e article2
+Medium
+InuNatNat — 12/17/2021
+https://natdecode.wordpress.com/
+Nat Decode
+Natalie Thompson
+Nat Decode
+Explain coding related concepts and share projects
+Image
+InuNatNat — 12/22/2021
+Def do Medium 1 and 2
+
+If you have more time work through easy 3, 4, 5
+Rona — 12/22/2021
+push all of them to GitHub
+annemelody — 01/04/2022
+https://docs.google.com/spreadsheets/d/1sIoaQsOBMf_0_d-SznKAri5ps4MfHg2ylNRNjnIAw4E/edit?usp=sharing
+Google Docs
+Type Coercion
+Sheet1
+
+Operator,What it converts the data to/Explanation,Data Type to Coerce,Example,Result,Notes
+Unary plus (+),Number,String,+('123'),123
++(''),0
++(' '),0
++('\n'),0
++('a'),NaN
++('1a'),NaN
+Boolean,+(true),1
++(false),0
+Null,+(null),0
+Undefined,+(undefined),NaN
+Binary plus (+) w two operands,Stri...
+Image
+annemelody — 01/09/2022
+https://docs.google.com/document/d/1RBAt_ku1JNhWtQE08dhM-OE224PbHevdyDP3r-PrYxU/edit?usp=sharing
+Google Docs
+Copy of JS211 Written Assessment Study Guide
+Specific Topics of Interest You should be able to clearly explain the following topics: assignments and comparison When used as a conditional, assignment always returns a truthy value. It’s best to avoid using assignments in a conditional. Both are expressions. Assignment The assignment ope...
+Image
+InuNatNat
+ pinned 
+a message
+ to this channel. See all 
+pinned messages
+.
+ — 01/09/2022
+InuNatNat — 01/23/2022
+///////////////////////
+InuNatNat — 01/23/2022
+\\\\\\\\\
+Rona — 01/23/2022
+?
+InuNatNat — 01/23/2022
+Sorry cats
+Rona — 01/23/2022
+i thought so
+Rona — 01/24/2022
+https://app.coderpad.io/TWNMKFN6
+JavaScript | CoderPad
+An online IDE for technical interviews offering a better candidate experience.
+
+Rona — 02/02/2022
+Homework for Wednesday February 2nd: Watch Problem 1 video, problem 2 video, and make sure test cases work for problem 2, and then do the whole PEDAC thing for problem 3
+Rona — 02/03/2022
+Rona's homework: Rewrite algo for problem 3 the one I intended fix code to accept Anne and Natalie's test cases, work on Edabit problems of category algorithms, array, string, numbers, objects, regex, sorting, validation 
+Rona — 02/03/2022
+For everyone for Thursday February 3rd: watch video 3, do PEDAC of problem 4 
+Rona — 02/04/2022
+Homework for Friday February 4th: Rona needs to do mini problem and solve problem Natalie gave.  Anne is going to finish problem 4.  Try problem 5.  Watch video 4 and video 5 if you want?
+Princess — 02/05/2022
+💪
+Rona — 02/07/2022
+Homework for Monday February 7th: Finish problem 5, try to do 1000 lights problem, Rona DON'T do 1000 lights 
+Rona — 02/08/2022
+Homework for Tuesday February 8th: Keep trying to finish problem 5.  Anne do Diamonds, Natalie look over Know My ABCs.  Rona do Know My ABC's.  Rona finish Diamonds.
+InuNatNat — 02/09/2022
+- I think it could help in the example phase, Instead of reading there explaination of the example. To write out your own explaination and compare it to theirs to make sure your understanding matches the provided explaination.
+- When you were writing the helper function part of your array you wrote "return true" where as in the code you wrote return []. Maybe just update it in your algo if you notice you were wrong originally.
+- Make sure when testing your helper function test both cases that will pass and fail.
+- After you realized that "oh we need to do n+1" you probably should have confirmed that the code looked how you were imagining it.
+- When you move on from building the array, you do not follow your algorithm. In the algorithm you wrote "At array[0], assign "irrelevant". You did not do that in your code. You should either make sure your code matches your algorithm or update the algorithm if you're understanding of the problem changes.
+- Saying "I think that this is it", but you haven't finished all of the logic written in your algorithm.
+- Make sure that the output you want at each part is right before you start the next part of the algorithm. (switching the lights on and off was not working properly when you started filtering).
+- Pay attention to === and = (equality vs Reassignment)
+- If you are getting the wrong output. Log all your variables and make sure they match what you think they should
+- When you were being thrown off by the 0 index and having to add slice to your code it is because you ignored a part of your algorithm, - too much hack and slash.
+- Be sure to understand the array methods, such as filter, map
+Rona — 02/09/2022
+Biggest Mistakes: if they give you a very thorough example, understand it YOURSELF first before reading their description
+In the examples section, write out what's happening
+Anne: When you're breaking down a specific example and writing down the steps to get to the output, that will help get to the algorithm in the code
+Rona — 02/09/2022
+Homework for Wednesday February 9th. Try to do Medium Problems 1 : Rotation 1, 2, 3
+Rona — 02/10/2022
+Agenda for Thursday February 10th: Rona do Caeser Cipher and Vigenere Cipher, Natalie do Caeser Cipher and Seeing Stars, Ann do Seeing Stars and Vigenre Cipher 
+Rona — 02/10/2022
+Homework for Thursday February 10th: Finish assigned problems, do 4 and 5 in medium problems which are stack machine interpretation and word to digit
+Rona — 02/11/2022
+Homework for Friday February 11th: Finish Fibonacci 1, 2, and 3, Bubble Sort from Medium 2 Natalie will do in front of us: Triangle Sides #2 and Unlucky Days #4.  Rona will do in front of us Letter Percentage Ratio and Next Featured Number Higher than a Given Value (or #5), Anne will do in front of us Triangles and Square Sum (so don't do them for homework the ones you're gonna do in front of us) 
+Rona — Today at 12:14 PM
+Rona does: Triangle Sides #2, Triangle // Unlucky Days 4, and Square Sum 
+Rona — Today at 1:34 PM
+Natalie does Letter Percentage Radio and Triangles // next featured number square sum 
+Anne does letter percentage ratio and triangle sides 2, //unlucky days 4 and next featured number higher than a given value 
+Rona — Today at 3:30 PM
+ask Natalie about implicit arguments array
+// Triangle Sides
+// A triangle is classified as follows:
+
+// Equilateral: All three sides are of equal length.
+// Isosceles: Two sides are of equal length, while the third is different.
+// Scalene: All three sides are of different lengths.
+// To be a valid triangle, the sum of the lengths of the two shortest sides must be greater than the length of the longest side, and every side must have a length greater than 0. If either of these conditions is not satisfied, the triangle is invalid.
+
+// Write a function that takes the lengths of the three sides of a triangle as arguments and returns one of the following four strings representing the triangle's classification: 'equilateral', 'isosceles', 'scalene', or 'invalid'.
+
+// Examples:
+/*
+Problem: Write a function that takes the lengths of the 3 sides of a triangle as arguments and returns  one of the following four strings representing the triangle's classification.
+
+rules/requirements:
+explicit:
+1) Equilateral means all 3 sides are of equal length.
+2) Isosceles: 2 sides are of equal length, 3rd different.
+3) Scalene - all 3 sides are of differeng lengths.
+4) Valid Triangle - sum of the lengths of the 2 shortest sides must be greater than the length of the longest side AND every side must have a length greater than 0. 
+5) 
+
+implicit:
+1) Will always be given 3 arguments.
+2) Can be given non-integer numbers.
+3) Can be given 0 as an argument, but that's an invalid input anyways.
+4) 
+
+input: 3 numbers
+output: string or false
+
+questions:
+1) What if pass in invalid data type?( Return false)
+2) What if pass in non integer number? (It's an acceptable number as long as it's greater than 0.)
+3) What if you pass in too many arguments?  (only use first 3)
+4) What if yu pass in not enough arguments? (Return false)
+5) What if you pass in Infinity? (Return false)
+6) What about NaN? (It's not a valid number, so return false)
+
+Examples/test cases:
+console.log(triangle(3, 3, 3));        // "equilateral"
+console.log(triangle(3, 3, 1.5));      // "isosceles"
+console.log(triangle(3, 4, 5));        // "scalene"
+console.log(triangle(0, 3, 3));        // "invalid" // invalid because one of the 3 sides is 0
+console.log(triangle(3, 1, 1));        // "invalid" // invalid because sum of 2 sides is less than 3rd
+console.log(triangle(3, 4, 5, 6)); // "scalene"
+
+console.log(triangle()) // false
+console.log(triangle('', 3, 6)) // false
+console.log(triangle([], 2, 3)) // false
+console.log(triangle({}, 5, 6)) // false
+console.log(triangle(undefined, 5, 6)) // false
+console.log(triangle(3, 4, 5, 6)) "scalene"
+console.log(triangle((2, 3))) // false
+console.log(triangle((Infinity, 4, 5))) // false
+
+Data structures:
+
+Input:  3 positive numbers
+
+Intermediate data structures: 
+
+output: String indicating equilateral, isosceles, scalene, or "invalid"
+
+Algorithm:
+
+-Declare a function `triangle` that takes 3 numbers `num1`, `num2`, `num3`.
+  -Declare `holder` to [...args] and hold all 3 arguments
+  -Pass `holder` to `isValidDataType` to make sure all elements are numbers and NOT Infinity and greater than 0
+
+  -Pass `holder` to `isValidTriangle`
+  
+  -if num1 === num2 === num3
+    return "equilateral"
+  -if num1 === num2 || num2 === num3 || num1 === num3
+    return "isosceles"
+  -else return "scalene"
+  
+ 
+-Helper function `isValidTriangle`
+  -Must check that the sum of 2 sides is greater than the third side
+  -if first and second side are less than 3rd side, return false
+  -if second and third side are less than first side, return false
+  -if first and third side are less than second side, return false
+  -otherwise return true
+  
+  
+-Helper function `isValidDataType`
+  -Check all elements are numbers
+  -Check all numbers are NOT Infinity
+  -Check all numbers are greater than 0.
+  -Check number is NOT NaN.
+
+*/
+
+function triangle(num1, num2, num3) {
+  console.log(num1, "num1", num2, "num2", num3, "num3")
+  let holder = [...arguments]
+  if (holder.length === 0) return false;
+  if (holder.some(element => element === Infinity)) return false
+... (47 lines left)
+Collapse
+message.txt
+6 KB
+﻿
+// Triangle Sides
+// A triangle is classified as follows:
+
+// Equilateral: All three sides are of equal length.
+// Isosceles: Two sides are of equal length, while the third is different.
+// Scalene: All three sides are of different lengths.
+// To be a valid triangle, the sum of the lengths of the two shortest sides must be greater than the length of the longest side, and every side must have a length greater than 0. If either of these conditions is not satisfied, the triangle is invalid.
+
+// Write a function that takes the lengths of the three sides of a triangle as arguments and returns one of the following four strings representing the triangle's classification: 'equilateral', 'isosceles', 'scalene', or 'invalid'.
+
+// Examples:
+/*
+Problem: Write a function that takes the lengths of the 3 sides of a triangle as arguments and returns  one of the following four strings representing the triangle's classification.
+
+rules/requirements:
+explicit:
+1) Equilateral means all 3 sides are of equal length.
+2) Isosceles: 2 sides are of equal length, 3rd different.
+3) Scalene - all 3 sides are of differeng lengths.
+4) Valid Triangle - sum of the lengths of the 2 shortest sides must be greater than the length of the longest side AND every side must have a length greater than 0. 
+5) 
+
+implicit:
+1) Will always be given 3 arguments.
+2) Can be given non-integer numbers.
+3) Can be given 0 as an argument, but that's an invalid input anyways.
+4) 
+
+input: 3 numbers
+output: string or false
+
+questions:
+1) What if pass in invalid data type?( Return false)
+2) What if pass in non integer number? (It's an acceptable number as long as it's greater than 0.)
+3) What if you pass in too many arguments?  (only use first 3)
+4) What if yu pass in not enough arguments? (Return false)
+5) What if you pass in Infinity? (Return false)
+6) What about NaN? (It's not a valid number, so return false)
+
+Examples/test cases:
+console.log(triangle(3, 3, 3));        // "equilateral"
+console.log(triangle(3, 3, 1.5));      // "isosceles"
+console.log(triangle(3, 4, 5));        // "scalene"
+console.log(triangle(0, 3, 3));        // "invalid" // invalid because one of the 3 sides is 0
+console.log(triangle(3, 1, 1));        // "invalid" // invalid because sum of 2 sides is less than 3rd
+console.log(triangle(3, 4, 5, 6)); // "scalene"
+
+console.log(triangle()) // false
+console.log(triangle('', 3, 6)) // false
+console.log(triangle([], 2, 3)) // false
+console.log(triangle({}, 5, 6)) // false
+console.log(triangle(undefined, 5, 6)) // false
+console.log(triangle(3, 4, 5, 6)) "scalene"
+console.log(triangle((2, 3))) // false
+console.log(triangle((Infinity, 4, 5))) // false
+
+Data structures:
+
+Input:  3 positive numbers
+
+Intermediate data structures: 
+
+output: String indicating equilateral, isosceles, scalene, or "invalid"
+
+Algorithm:
+
+-Declare a function `triangle` that takes 3 numbers `num1`, `num2`, `num3`.
+  -Declare `holder` to [...args] and hold all 3 arguments
+  -Pass `holder` to `isValidDataType` to make sure all elements are numbers and NOT Infinity and greater than 0
+
+  -Pass `holder` to `isValidTriangle`
+  
+  -if num1 === num2 === num3
+    return "equilateral"
+  -if num1 === num2 || num2 === num3 || num1 === num3
+    return "isosceles"
+  -else return "scalene"
+  
+ 
+-Helper function `isValidTriangle`
+  -Must check that the sum of 2 sides is greater than the third side
+  -if first and second side are less than 3rd side, return false
+  -if second and third side are less than first side, return false
+  -if first and third side are less than second side, return false
+  -otherwise return true
+  
+  
+-Helper function `isValidDataType`
+  -Check all elements are numbers
+  -Check all numbers are NOT Infinity
+  -Check all numbers are greater than 0.
+  -Check number is NOT NaN.
+
+*/
+
+function triangle(num1, num2, num3) {
+  console.log(num1, "num1", num2, "num2", num3, "num3")
+  let holder = [...arguments]
+  if (holder.length === 0) return false;
+  if (holder.some(element => element === Infinity)) return false
+  if (!isValidDataType(holder)) return false;
+  if (!isValidTriangle(holder)) return "Invalid";
+  
+  console.log(holder)
+  if (holder.every(element => element === holder[0])) {
+    return "equilateral"
+  } else if (holder[0] === holder[1] || holder[0] === holder[2] || holder[1] === holder[2] ) {
+    return "isosceles"
+  } else {
+    return "scalene"
+  }
+}
+
+function isValidTriangle(holder) {
+  if (holder[0] + holder[1] < holder[2]) {
+    return false
+  } else if (holder[1] + holder[2] < holder[0]) {
+    return false
+  } else if (holder[0] + holder[2] < holder[1]) {
+    return false
+  } 
+  return true;
+}
+
+function isValidDataType(holder) {
+  return holder.every(element => {
+    if (element !== Infinity && element > 0 && typeof(element) === 'number' && !isNaN(element)) return true;
+    return false;
+  })
+}
+
+// console.log(triangle(3, 3, 3));        // "equilateral"
+// console.log(triangle(3, 3, 1.5));      // "isosceles"
+//  console.log(triangle(3, 4, 5));        // "scalene"
+// console.log(triangle(0, 3, 3));        // false // invalid because one of the 3 sides is 0
+// console.log(triangle(3, 1, 1));        // "invalid" // invalid because sum of 2 sides is less than 3rd
+// console.log(triangle(3, 4, 5, 6)); // "scalene"
+// console.log(triangle(3, 4, 5, 6)) //"scalene"
+// console.log(triangle(3, 4, NaN)) // false because NaN
+
+//console.log(triangle()) // false
+// console.log(triangle('', 3, 6)) // false
+// console.log(triangle([], 2, 3)) // false
+// console.log(triangle({}, 5, 6)) // false
+// console.log(triangle(undefined, 5, 6)) // false
+console.log(triangle((2, 3))) // false
+console.log(triangle((Infinity, 4, 5))) // false
+message.txt
+6 KB
